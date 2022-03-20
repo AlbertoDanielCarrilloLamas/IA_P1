6e7474b8ba37aa47fd8639db802ae52a1984b13a
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:25:14 -0600

    Practica 7
    Calcular potencias en Python

---
+++ _7._potencias.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+potencia = 2**10 #calculamos la decima potencia con el doble asterisco (**) y almacenamos el resultado en una variable
+print(potencia) #imprimimos el resultado
+
+mul=2*2*2*2*2*2*2*2*2*2 #calculamos la decima potencia sin el operando
+print(mul) #imprimimos el resultado
