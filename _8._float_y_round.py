46a248da391d5a7898bc84554a7a5e360908f46c
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:25:49 -0600

    Practica 8
    Redondear los decimales al imprimir un flotante

---
+++ _8._float_y_round.py
@@ -0,0 +1,5 @@
+#programa:  Redondea con cinco decimales el número 17.567383292929200234.
+
+num = 17.567383292929200234 # declaramos la variable de tipo float
+
+print(round(num,5)) #utilizamos el round para decir cuantos decimales muestre en el print
