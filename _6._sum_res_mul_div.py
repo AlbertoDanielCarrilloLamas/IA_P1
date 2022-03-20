d01ee1697f009dcc20ca3b30cc692f1c0bae8506
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:24:52 -0600

    Practica 6
    Hacer uso de las operaciones basicas: suma, resta, multiplicación, división

---
+++ _6._sum_res_mul_div.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+var87 = 28+23+36 #sumamos con el operando '+' valores para que nos de un resultado de 87
+print(var87)
+
+var_87 = 28+23-28-23-87 #restamos con el operando '-' valores para que nos de un resultado de -87
+print(var_87)
+
+mul870 = 20*23*1.8914 #multiplicamos con el operando '*' valores para que nos de un resultado de 870
+print(mul870)
+
+div10 = 5000/230/2 #dividimos con el operando '/' valores para que nos de un resultado de 10.
+print(div10)
+
+#despues de cada operacion imprimimos el resultado almacenado en la variable
