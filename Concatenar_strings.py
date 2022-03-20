648e37fb86b47b83c304952eb094e69de96ca1f6
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:11:08 -0600

    Practica 2
    Strings con comillas simples y dobles

---
+++ Concatenar_strings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+#lo de arriba es para usar acentos :)
+
+string1 = "Hola" #declaramos string 1 y 2
+string2 = 'mundo'
+string3 = string1+string2 #concatenamos los dos strings con el operando '+' almacenando en una tercer variable
+print(string3)
+
+string1 = string1 + string2 #concatenamos los strings reciclando una variable
+print(string1)
+
+print("Hola " + string2 + " :)") #concatenamos strings con otra variable dentro de un print
---
+++ String_comillas_s_y_d.py
@@ -0,0 +1,7 @@
+var1 = "String con comillas dobles" #declaramos variables con comillas dobles
+var2 = 'String con comillas simples'#declaramos variables con comillas simples
+
+print(var1); print(var2) #imprimimos las dos variables en una sola linea
+
+frase = '"print()" se utiliza para imprimir valores en la consola' #almacenamos una frase que contenga comillas
+print(frase) #imprimimos la frase
