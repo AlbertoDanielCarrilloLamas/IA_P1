85de4219e0c75195c25409748263876fc2ab16c2
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:27:38 -0600

    Practica 12
    Eliminar elementos de una lista con la función 'remove'

---
+++ _12._eliminar_con_remove.py
@@ -0,0 +1,10 @@
+# Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'.
+# Solo puedes eliminarlos utilizando el método remove().
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#declaramos lista
+
+colores.remove('amarillo') #eliminamos el amarillo
+colores.remove("rojo") #eliminamos el rojo
+
+print(colores) #corroboramos que se hayan eliminado los elementos
