ed0310bfa4ee937186353c51a36cf827a280b96d
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:28:33 -0600

    Practica 14
    Añadir elementos a una lista

---
+++ _14._Insertar_append.py
@@ -0,0 +1,9 @@
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#creamos la lista colores
+
+colores.append('fuxia') #añadimos color con comillas simples
+colores.append("celeste")#añadimos color con comillas dobles
+
+print(colores) #mostramos lalista con los elementos agregados
