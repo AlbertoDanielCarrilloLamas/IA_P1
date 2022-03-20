8bbb784d7cb31f0af04214d27102e4a7a3430400
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:13:57 -0600

    Practica 4
    Convertir a mayusculas y minusculas con las funciones upper and lower

---
+++ _4._title_upper_lower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+nombre = "carrillo lamas alberto daniel" #declaramos un strings en minusculas
+print(nombre.title()) #imprimimos el string con el formato de titulo (mayuscula la primer letra de cada palabra)
+
+frase = "Esta Es Una Frase Para Ser Formateada." #declaramos string con mayusculas y minusculas
+
+print(frase.upper()) #imprimimos string en mayusculas
+print(frase.lower()) #imprimimos string en minusculas
