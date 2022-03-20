17f9b0ecd9a756f830bdc381f2ca379e3a15d4e6
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-03-20 12:14:24 -0600

    Practica 5
    Saltos de linea y tabulacion

---
+++ _5._Salto_linea_y_tabulacion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ cdoing: 850 _*_
+# _*_ coding: utf-8 _*_
+
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.")
+#imprimimos las palabras con el salto de linea (\n)
+
+print("\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.")
+#imprimimos, además del salto, con una tabulación(\t)
