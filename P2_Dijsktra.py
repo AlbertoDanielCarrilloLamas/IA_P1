2ddaf990a90699da67280bfb5044b52a7b6be4cb
AlbertoDanielCarrilloLamas <101990868+AlbertoDanielCarrilloLamas@users.noreply.github.com>
2022-06-01 08:48:24 -0500

    Update P2_Dijsktra.py

--- P2_Dijsktra.py
+++ P2_Dijsktra.py
@@ -9,7 +9,8 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import random
+"""
 datos = pd.read_csv('Nodos y caminos.csv') #importamos datos del archivo csv (creacion de dataframe) con ayuda de panda
 datos.head() #mostramos dataframe
 
@@ -24,6 +25,32 @@ djk_path #indicamos origen y destino y con la funcion dijkstra_path encontramos 
 
 mejor_ruta = Rutas.subgraph(['Casa', 'Base aerea', 'Real center', 'Trompo magico', 'Juan palomar','Estadio 3 de marzo','Pablo neruda','CETI']) #almacenamos la mejor ruta calculada
 nx.draw(mejor_ruta,with_labels=True) #dibujamos la mejor ruta
+plt.show() #mostramos grafo"""
+
+#Grafo
+Grafo = nx.Graph() # creamos un grafo y añadimos nodos
+
+#Añadir aristas
+# G.add_edge(2, 3, weight=0.9) /// a = random.randint(1,5)
+e = [('Casa', 'Base aerea', random.randint(1,5)), ('Base aerea', 'Real center', random.randint(1,5)), ('Real center', 'Trompo magico', random.randint(1,5)), ('Trompo magico', 'Juan palomar', random.randint(1,5)), ('Juan palomar', 'Estadio 3 de marzo', random.randint(1,5)), ('Estadio 3 de marzo', 'Pablo Neruda', random.randint(1,5)), ('Pablo Neruda', 'CETI', random.randint(1,5)), ('Trompo magico', 'Acueducto', random.randint(1,5)), ('Acueducto', 'Pabellon', random.randint(1,5)), ('Pabellon', 'Pablo Neruda', random.randint(1,5)), ('Pabellon', 'Alberta', random.randint(1,5)), ('Alberta', 'CETI', random.randint(1,5)), ('Base aerea', 'La cima', random.randint(1,5)), ('La cima', 'Belenes', random.randint(1,5)), ('Belenes', 'Zapopan', random.randint(1,5)), ('Zapopan', 'Americas', random.randint(1,5)), ('Americas', 'Alberta', random.randint(1,5)), ('Zapopan', 'Plaza patria', random.randint(1,5)), ('Plaza patria', 'Alberta', random.randint(1,5)) ]
+
+Grafo.add_weighted_edges_from(e)
+print(nx.dijkstra_path(Grafo, 'Casa', 'CETI')) #mostrar en consola el mejor camino
+
+"""
+ejemplo Dijsktra internet
+G = nx.Graph()
+e = [('a', 'b', 0.3), ('b', 'c', 0.9), ('a', 'c', 0.5), ('c', 'd', 1.2)]
+G.add_weighted_edges_from(e)
+print(nx.dijkstra_path(G, 'a', 'd'))
+
+['a', 'c', 'd']  salida por consola
+
+"""
+#graficar gafo
+
+best = nx.dijkstra_path(Grafo, 'Casa', 'CETI')
+nx.draw(Grafo, node_size = 100, width = 1, with_labels = True, edge_color="tab:red")
 plt.show() #mostramos grafo
 
 
